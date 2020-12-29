e22f2292beecc8b655f420eb0ba07b70776aaf26
Maxime Boudart <mboudartpro@gmail.com>
2020-12-29 13:24:34 +0100

    Mise à jour des formulaires Employees

--- templates/Employees/edit.php
+++ templates/Employees/edit.php
@@ -18,17 +18,24 @@
     </aside>
     <div class="column-responsive column-80">
         <div class="employees form content">
-            <?= $this->Form->create($employee) ?>
+            <?= $this->Form->create($employee, ['enctype' => 'multipart/form-data']) ?>
             <fieldset>
                 <legend><?= __('Edit Employee') ?></legend>
                 <?php
                     echo $this->Form->control('birth_date');
                     echo $this->Form->control('first_name');
                     echo $this->Form->control('last_name');
-                    echo $this->Form->control('gender');
                     echo $this->Form->control('hire_date');
+                    echo $this->Form->label('gender', 'Gender');
+                    echo $this->Form->select('gender', [
+                            'M' => 'Homme',
+                            'F' => 'Femme',
+                            'X' => 'Autre',
+                        ], 
+                    );
                     echo $this->Form->control('email');
                     echo $this->Form->control('password');
+                    echo $this->Form->control('picture', ['type' => 'file']);
                 ?>
             </fieldset>
             <?= $this->Form->button(__('Submit')) ?>
